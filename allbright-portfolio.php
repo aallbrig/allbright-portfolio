d8aad4eaf2794e9675fed87931fed05813e6a53a
Andrew Allbright <aallbrig@Andrews-MacBook-Pro.local>
2014-12-03 18:05:59 -0500

    Added primary plugin file

---
+++ allbright-portfolio.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Allbright Custom Portfolio
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: My very own custom portfolio experience
+ * Version: 1.0.0
+ * Author: Andrew Allbright
+ * Author URI: http://www.andrewallbright.com
+ * License: GPL2
+ */
